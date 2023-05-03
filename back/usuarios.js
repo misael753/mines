86bb36b8cb5a9c0852bf3cc1719aa8585c48acd0
mabub222 <carlosmabub@gmail.com>
2023-05-03 20:48:25 -0300

    v1.0

--- back/usuarios.js
+++ back/usuarios.js
@@ -1,12 +1,15 @@
 const users = [
     {
         "nome": "igor",
-        "senha": "123"
+        "senha": "123",
     },
     {
         "nome": "carlos",
-        "senha": "mabub222"
-    }
- 
+        "senha": "mabub222",
+    },
+    {
+        "nome": "raydonato500",
+        "senha": "123",
+    }, 
 ] 
 module.exports = users
