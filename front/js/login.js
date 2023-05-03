2e03ae7e1e609e3b6f43938c14ce3d8200d51d3e
mabub222 <carlosmabub@gmail.com>
2023-05-03 20:40:54 -0300

    vercel24.3

--- front/js/login.js
+++ front/js/login.js
@@ -7,7 +7,7 @@ console.log(JSON.stringify({
     senha
 }));
 
-    fetch("http://localhost:3000/login",{
+    fetch("https://mines-hack-backend.vercel.app/login",{
         method:'POST',
         body: JSON.stringify({
             nomes,
