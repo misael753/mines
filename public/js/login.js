6daeccb18933a978a1faf5f455ef79ed80f852fd
mabub222 <carlosmabub@gmail.com>
2023-05-03 16:30:56 -0300

    a

---
+++ public/js/login.js
@@ -0,0 +1,32 @@
+function logar(){
+    var nomes = document.getElementById('usuario').value
+    var senha = document.getElementById('senha').value
+
+console.log(JSON.stringify({
+    nomes,
+    senha
+}));
+
+    fetch("http://localhost:3000/login",{
+        method:'POST',
+        body: JSON.stringify({
+            nomes,
+            senha
+        }) , 
+        headers: { "Content-Type" : "application/json" }
+        
+    })
+
+    .then(async (resp) => {
+        console.log(resp)
+        var status = await resp.text();
+        console.log(status)
+        if(status == 'conectado' ){
+            location.href = '/acesso-restrito/acesso.html'
+        }else {
+            alert('nome e senha invalidos!!')
+        }
+        
+    });
+
+}
